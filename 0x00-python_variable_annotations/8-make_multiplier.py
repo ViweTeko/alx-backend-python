6fab0e925ab275e0f1b4d032df765db3f583cfe8
ViweTeko <iamviwe.teko@gmail.com>
2024-07-05 00:53:59 +0200

    Indentation fixed

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,4 +5,4 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ This function returns function that multiplies"""
-        return lambda x: x * multiplier
+    return lambda x: x * multiplier
