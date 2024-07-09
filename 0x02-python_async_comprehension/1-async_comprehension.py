b57ac65e5d3eb5b5933bcef742db815c6e0268fc
ViweTeko <iamviwe.teko@gmail.com>
2024-07-09 20:18:34 +0200

    Redone

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers and return them"""
-    return [a async a in async_generator()][:10]
+    return [a async for a in async_generator()][:10]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Continuation of previous script"""
+import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Measure runtime"""
-    start = asyncio.get_event_loop().time()
-    await asyncio.gather(*(async_comprehension() for a in range(4)))
-    end = asyncio.get_event_loop().time()
-    return end - start
+    start = time.perf_counter()
+    task = [async_comprehension() for a in range(4)]
+    await asyncio.gather(*task)
+    end = time.perf_counter()
+    return (end - start)
